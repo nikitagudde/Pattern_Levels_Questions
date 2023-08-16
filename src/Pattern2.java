e1092064f702d8287188c65d0bd6ef46a15c6d64
nikitagudde <nikitagudde663@gmail.com>
2023-08-16 13:08:59 +0530

    first commit

---
+++ src/Pattern2.java
@@ -0,0 +1,11 @@
+public class Pattern2 {
+	public static void main(String args[]) {
+		int i, j ,n=4;
+		for (i = 4; i >= 1; i--) {
+			for (j = 1; j <= i; j++) {
+				System.out.print("# ");
+			}
+			System.out.println();
+		}
+	}
+	}
